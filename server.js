4782d4ff719ac2f16eacacbe5f71b813e436f612
coquispe1-cpu <coquispe1@gmail.com>
2025-08-23 11:46:27 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
-// Esta línea es la clave para solucionar el error
-app.use(express.static(path.join(__dirname, 'src', 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta principal para servir index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Inicia el servidor
-app.listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port}`);
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
 });
