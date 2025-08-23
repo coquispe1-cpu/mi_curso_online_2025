a0a11bf063c1e274f48840b833c68360d38ea305
coquispe1-cpu <coquispe1@gmail.com>
2025-08-23 11:37:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Configura la ruta estática para servir los archivos de la carpeta 'public'
-app.use(express.static(path.join(__dirname, 'public')));
+// Esta línea es la clave para solucionar el error
+app.use(express.static(path.join(__dirname, 'src', 'public')));
 
 // Ruta principal para servir index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'src', 'public', 'index.html'));
 });
 
 // Inicia el servidor
