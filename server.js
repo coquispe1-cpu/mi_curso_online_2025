00315ac1c3b109369b58d69906f4b90d9e10e2d7
coquispe1-cpu <coquispe1@gmail.com>
2025-08-23 10:14:44 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 10000;
 
+// Servir archivos estáticos desde la carpeta public
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Ruta principal
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
-});
+})
