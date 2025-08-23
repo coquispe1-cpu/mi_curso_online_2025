56474d54aa79dc8351c0e2f192ceec137d0fccae
coquispe1-cpu <coquispe1@gmail.com>
2025-08-23 13:53:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require('express');
+const express = require('require');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Esta línea le dice a Express que sirva archivos estáticos desde la carpeta 'src/public'
 app.use(express.static(path.join(__dirname, 'src', 'public')));
 
+// Esta ruta manejará las solicitudes a la página de inicio y enviará 'index.html'
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'src', 'public', 'index.html'));
 });
