04b4fe83d97190b83ccab50ebe6c12b3300fb2c2
coquispe1-cpu <coquispe1@gmail.com>
2025-08-23 23:20:15 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-// Esta línea le dice a Express que sirva archivos estáticos desde la carpeta 'src/public'
-app.use(express.static(path.join(__dirname, 'src', 'public')));
+// Sirve archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Esta ruta manejará las solicitudes a la página de inicio y enviará 'index.html'
+// Redirige la ruta raíz '/' para servir el index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port}`);
+// Inicia el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
