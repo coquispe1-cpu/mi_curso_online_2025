b264ee75d7a6393943dcc46b6c88c4444cfc67ee
coquispe1-cpu <coquispe1@gmail.com>
2025-08-25 09:13:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Carpeta de archivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Puerto
+// Puerto (Render usará process.env.PORT)
 const PORT = process.env.PORT || 3000;
+
+// Carpeta pública donde está tu HTML, CSS y videos
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Redirige todas las rutas a index.html (opcional)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// Inicia el servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
